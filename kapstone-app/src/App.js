bcca56c44e10408537e1f98d61642201a0ddd1af
Joel Rice <41754404+JoelRice@users.noreply.github.com>
2021-04-06 14:20:34 -0400

    Basic setup up of kapstone-frontend done

---
+++ kapstone-app/src/App.js
@@ -0,0 +1,16 @@
+import "./assets/App.css";
+// import create from "zustand";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
