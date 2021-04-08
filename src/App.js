6adec6e21663dcfaa295a891f2af8399fc79de3a
RosieR403 <RosieR403@gmail.com>
2021-04-08 06:14:04 -0500

    working on profile/sign-up/appbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./assets/App.css";
 import React from "react";
 import { Button } from "@material-ui/core";
+import menuHeader from "./views/header";
 
 // import create from "zustand";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MenuHeader />
         <p>
           <Button color="secondary">Press me!</Button>
         </p>
