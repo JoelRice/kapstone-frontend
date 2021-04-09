09021d63c591b9b34484fae2e070b5d3f8117616
Joel Rice <41754404+JoelRice@users.noreply.github.com>
2021-04-09 11:00:32 -0400

    doing more cleanup

--- src/store/store.js
+++ src/store/store.js
@@ -6,6 +6,9 @@ const initialState = {
   token: "",
   toast: {
     text: "",
+    statusCode: 0,
+    color: "",
+    //still need to sort out how colors work here
     //   color/statusCode/whatever: ''
   },
 };
@@ -13,9 +16,38 @@ const initialState = {
 export const actions = {
   LOGIN: "LOGIN",
   LOGOUT: "LOGOUT",
+  TOAST: "TOAST",
+  UNTOAST: "UNTOAST",
 };
 
+const toastFor = (action, successMessage, successCode = 0) => ({
+  message: action.payload?.message || successMessage,
+  statusCode: action.payload?.statusCode || successCode,
+});
+
 // define reducer function
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.LOGIN:
+      return {
+        user: action.payload,
+        toast: toastFor(
+          action,
+          `Successfully logged in as ${action.payload.username}`
+        ),
+      };
+    case actions.LOGOUT:
+      return {
+        user: initialState.user,
+        toast: toastFor(action, "Log out successful", 200),
+      };
+    case actions.TOAST:
+      return { toast: { ...state.toast, ...action.payload } };
+    case actions.UNTOAST:
+      return { toast: initialState.toast };
+    default:
+      return state;
+  }
+};
 // create useStore hook
 export const useStore = create(devtools(redux(reducer, initialState)));
