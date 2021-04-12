6faf14f4615c116c12976ca0801f67de1c1cd72b
Joel Rice <41754404+JoelRice@users.noreply.github.com>
2021-04-12 12:04:51 -0400

    Testing endpoints

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Button } from "@material-ui/core";
 import MenuHeader from "./views/header";
@@ -7,26 +6,23 @@ import Admin from "./views/admin.js";
 import { Route, Switch } from "react-router-dom";
 import SignUpPage from "./views/signup";
 import LoginPage from "./views/login";
-
-
-
+import Toast from "./views/toast";
 function App() {
   return (
     <div className="App">
-      
       <header className="App-header">
-        <Route path="/:page?" render={(history) => {
-          return (
-            <MenuHeader  {...history}/>
-            )
-        }}
+        <Toast />
+        <Route
+          path="/:page?"
+          render={(history) => {
+            return <MenuHeader {...history} />;
+          }}
         />
         <Switch>
-        <Route exact path="/admin" component={Admin}/>
-        <Route exact path="/signup" component={SignUpPage}/>
-        <Route exact path="/login" component={LoginPage}/>
+          <Route exact path="/admin" component={Admin} />
+          <Route exact path="/signup" component={SignUpPage} />
+          <Route exact path="/login" component={LoginPage} />
         </Switch>
-
       </header>
     </div>
   );
