09e59f732853b543c9cf9ed5aa270c32b956ccf4
RosieR403 <RosieR403@gmail.com>
2021-04-15 09:24:29 -0500

    moving code to correct component

--- src/views/users.js
+++ src/views/users.js
@@ -1,58 +1,11 @@
 import { Container, Typography, Grid, Box, Card, CardActions, Button } from "@material-ui/core";
 import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import UserCard from "../components/userCard";
 
 function Users(props) {
-  // const history = useHistory();
-  // const dispatch = useStore((state) => state.dispatch);
-  const {userKitties, userItems, userBio, userWishList, match, history } = props
-  const { params } = match;
-  const { username } = params;
-
-  useEffect () => {
-    return (
-      console.log(params)
-    )
-  }
-
   return (
-  <div className="users-page">
-    <Container style={{ backgroundColor: "#cfe8fc",}}>
-      <Typography>
-        User's Profile
-      </Typography>
-      <Card>
-        <CardActions>
-          <Button>
-            Request Trade
-          </Button>
-        </CardActions>
-      </Card>
-      <Grid container 
-      direction="row"
-      justify="center"
-      alignItems="stretch"
-      >
-      <Grid item>
-      <Typography>
-        User's Kitties
-      </Typography>
-      </Grid>
-      <Grid item>
-      <Typography>
-        User's Wishlist
-      </Typography>
-      </Grid>
-      <Grid item>
-      <Typography>
-        User's Items
-      </Typography>
-      </Grid>
-      </Grid>
-    </Container>
-    
-  </div>
-  console.log(username)
+  <UserCard/>
   );
 };
 
