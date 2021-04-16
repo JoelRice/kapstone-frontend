2cd8058e1151a07318e4a53d7c697801bc709d2f
Joel Rice <41754404+JoelRice@users.noreply.github.com>
2021-04-16 10:38:47 -0400

    added repo to browser router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ console.log(Requests);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/<repo>">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
